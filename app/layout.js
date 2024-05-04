b5f0e36eca140d9fe8b457b1b6de01b78f052fe5
Samya Saha <77977204+Samya-S@users.noreply.github.com>
2024-05-04 18:39:11 +0530

    chore: Add ErrorBoundary component to RootLayout - post build application error fix 5

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import './globals.css'
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
 import StoreProvider from './StoreProvider'
+import ErrorBoundary from '@/components/ErrorBoundary'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,11 +16,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <StoreProvider>
-          <Navbar />
-          {children}
-          <Footer />
-        </StoreProvider>
+        <ErrorBoundary>
+          <StoreProvider>
+            <Navbar />
+            {children}
+            <Footer />
+          </StoreProvider>
+        </ErrorBoundary>
       </body>
     </html>
   )
