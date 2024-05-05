55272da0da1b5301f7a8c0e1e885a57a1e7cd818
Samya Saha <77977204+Samya-S@users.noreply.github.com>
2024-05-05 15:49:48 +0530

    refactor: Improve database connection handling in middleware/connectToDB.js

--- middleware/connectToDB.js
+++ middleware/connectToDB.js
@@ -1,33 +1,29 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-    if (mongoose.connections[0].readyState) {
-        console.log("MongoDB connected already")
-        return true;//
-    }
-    else {
-        try {
-            await mongoose.connect(process.env.MONGO_URI);
-            console.log("MongoDB connection began");
+let cached = global.mongoose;
 
-            const connection = mongoose.connection;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
-            connection.on('connected', () => {
-                console.log("MongoDB connected successfully");
-                return true;//
-            })
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-            connection.on('error', (err) => {
-                console.log('MongoDB connection error' + err);
-                // process.exit();//
-                return false;//
-            })
-        }
-        catch (error) {
-            console.error(error);
-            return false;//
-        }
-    }
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 export default connectDB;
