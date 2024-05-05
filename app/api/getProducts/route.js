aad390b1a6e6e74d4c8e74d2c4bb0e6a223a49db
Samya Saha <77977204+Samya-S@users.noreply.github.com>
2024-05-05 16:01:17 +0530

    refactoring database connection handling in getProducts API route

--- app/api/getProducts/route.js
+++ app/api/getProducts/route.js
@@ -4,13 +4,7 @@ import connectDB from "@/middleware/connectToDB";
 export async function GET(request) {
     try {
         // Calls the connect function to establish a connection to the database.
-        const dbIsConnected = await connectDB();//
-        if (!dbIsConnected) {//
-            return Response.json({//
-                status: 500,//
-                body: { success: false, error: "Failed to connect to the database" },//
-            });//
-        }//
+        await connectDB();
 
         const products = await Product.find();
         return Response.json({
