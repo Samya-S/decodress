d14f3bf7ac42f52a24f789b8cf54af225e68e10e
Samya Saha <77977204+Samya-S@users.noreply.github.com>
2024-05-09 12:02:08 +0530

    refactor: Update User model to store phone number as a string

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    phone: { type: Number, default: 0},
+    phone: { type: String, default: ''},
     address: { type: String, default: '' },
     pincode: { type: String, default: '' },
 }, { timestamps: true });
