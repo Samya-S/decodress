04284135f37429b754f9638deddd6ba90e848085
Samya Saha <77977204+Samya-S@users.noreply.github.com>
2024-05-14 19:33:31 +0530

    refactor: Add NotFound component for handling 404 errors

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+      <div className="flex min-h-[60vh] w-auto justify-center items-center text-2xl md:text-3xl">
+        404 | Page not found
+      </div>
+    )
+  }
+  
+  export default NotFound
